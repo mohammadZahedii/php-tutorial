1741b896400faa6b1b2dbef2cb1c69f47b05001a
mohammadzahedii4713 <mohammadzahedii4713@gmail.com>
2023-04-13 16:46:38 +0330

    complete syntax in basic cases

--- index.php
+++ index.php
@@ -1,27 +1,82 @@
 <?php
-
-$first_name = "mohammad";
-$NumbeR1 = "10";
-$NumbeR2 = "12";
-$status = true;
-$a = 10245;
-$b = 25 / 7;
-
-
 //strings
-$first_name = 'mohammad\'s';
-$first_name = "mohammad's";
-$last_name = 'zahedi';
-$full_name = $first_name . " " . $last_name;
-$description = 'hi my name
- is mohammadreza
-i\'m 27 years old
-';
+
+// $first_name = "mohammad";
+// $NumbeR1 = "10";
+// $NumbeR2 = "12";
+// $status = true;
+// $a = 10245;
+// $b = 25 / 7;
+// $first_name = 'mohammad\'s';
+// $first_name = "mohammad's";
+// $last_name = 'zahedi';
+// $full_name = $first_name . " " . $last_name;
+// $description = 'hi my name
+//  is mohammadreza
+// i\'m 27 years old
+// ';
 // echo ($description);
 // echo 'You deleted: C:\\*.*?';
 // var_dump($description);
 // var_dump($full_name);
-var_dump("$first_name $last_name");
+// var_dump("$first_name $last_name");
 // var_dump((int)$b);
 // var_dump($status);
 // var_dump($a);
+
+//operators
+
+// $a = 10;
+// $b = 20;
+// $c = 0.344;
+// $a++;
+// $b++;
+// $d = --$a;
+// var_dump($d);
+// var_dump($a);
+
+// var_dump($a * $b);
+// var_dump((bool) ((int) $c));
+// var_dump($a % $b);
+// var_dump($a - $b);
+
+
+//Comparision Operators
+
+// $a = 10;
+// $b = "10";
+// var_dump($a <> $b);
+// var_dump(10 <=> 10);
+// var_dump(11 <=> 10);
+// var_dump(10 <=> 11);
+
+//logical Operators
+
+// $is_loggedIn = true;
+// $is_admin = false;
+// if ($is_loggedIn && $is_admin) {
+//     echo 'hello mohammad';
+// } else {
+//     echo 'please loggin';
+// }
+
+
+
+//Assignment Operators
+
+// $a = "reza";
+// $b = "mohammad";
+// $a ??= $b;
+// var_dump($a);
+
+//Array
+// $users = [
+//     1 => "mohammad",
+//     2 => "reza",
+//     3 => "zahedi"
+// ];
+// var_dump($users);
+
+//Constants 
+const lastName = "zahedi";
+var_dump(lastName);
